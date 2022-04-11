b6210a6ae98c0ef83893a184cfc346c9a3c3853d
Priest4612 <priest4612@gmail.com>
2022-04-12 02:05:23 +0300

    Обновил датаклассы

--- 03_sqlite_to_postgres/data_classes_models.py
+++ 03_sqlite_to_postgres/data_classes_models.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass, field
 
 
 @dataclass
-class Genre:
+class Genre(object):
     id: uuid.UUID = field(default_factory=uuid.uuid4)
     name: str
     description: str
@@ -13,13 +13,37 @@ class Genre:
 
 
 @dataclass
-class Filmwork:
+class Filmwork(object):
     id: uuid.UUID = field(default_factory=uuid.uuid4)
     title: str
     descriptions: str
     creation_date: datetime.date
-    rating: float = field(dafault=0.0)
+    rating: float = field(dafault=0)
     type: str
+    created: datetime.datetime
+    modified: datetime.datetime
 
 
 @dataclass
+class Person(object):
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
+    fill_name: str
+    created: datetime.datetime
+    modified: datetime.datetime
+
+
+@dataclass
+class GenreFilmwork(object):
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
+    genre: uuid.UUID
+    film_work: uuid.UUID
+    created: datetime.datetime
+
+
+@dataclass
+class PersonFilmWork(object):
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
+    person: uuid.UUID
+    film_work: uuid.UUID
+    role: str
+    created: datetime.datetime
